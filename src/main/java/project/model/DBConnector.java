b4dd16d44e1224c2e1d3af431971ae949668a227
gudkjon <gudkjon@gmail.com>
2015-10-19 12:48:36 +0000

    afsa

--- src/main/java/project/model/DBConnector.java
+++ src/main/java/project/model/DBConnector.java
@@ -13,7 +13,7 @@ public class DBConnector {
     private ArrayList<String> results;
     private ArrayList<Survey> surveys;
     public String user = "postgres";
-    public String password = "hoppilius4";
+    public String password = "Enter password here";
     public String dbLink = "jdbc:postgresql://localhost:5432/Surveys";
     public DBConnector() {
         c = null;
